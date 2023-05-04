2ef3d92d3920fcfce54d7fad7f468debc6dae05d
trantuvan <trantuvan.kan@gmail.com>
2023-05-04 18:12:04 +0700

    Add project files.

---
+++ TestProject/EnumerableDebugger.cs
@@ -0,0 +1,22 @@
+﻿namespace TestProject
+{
+    public static class EnumerableDebugger
+    {
+        public static IEnumerable<T> Watch<T>(this IEnumerable<T> input)
+        {
+            return Watch(input, item => item
+            != null ? item.ToString() : "(null)");
+        }
+
+        public static IEnumerable<T> Watch<T>(this IEnumerable<T> input, Func<T, string> toString)
+        {
+            foreach (var item in input)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine(toString(item));
+                Console.ResetColor();
+                yield return item;
+            }
+        }
+    }
+}
---
+++ TestProject/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace TestProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var res = typeof(string).GetMethods()
+                //.Watch(m => "SOURCE: " + m)
+                .Where(m => m.Name.StartsWith("S"))
+                .Watch(m => "WHERE: " + m.Name)
+                .Select(m => m.Name)
+                .Watch(m => "SELECT: " + m);
+
+            foreach (var name in res)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
